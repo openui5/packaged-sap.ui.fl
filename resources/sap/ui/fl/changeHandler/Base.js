c036b3ce834fa64ce2f613e65c59604fd652c225
OpenUI5 <openui5@sap.com>
2021-02-08 17:50:41 +0100

    1.52.47

--- resources/sap/ui/fl/changeHandler/Base.js
+++ resources/sap/ui/fl/changeHandler/Base.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2021 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -11,7 +11,7 @@ sap.ui.define(function() {
 	 * Base functionality for all change handler which provides some reuse methods
 	 * @alias sap.ui.fl.changeHandler.Base
 	 * @author SAP SE
-	 * @version 1.52.46
+	 * @version 1.52.47
 	 * @experimental Since 1.27.0
 	 *
 	 */
--- resources/sap/ui/fl/changeHandler/BaseRename.js
+++ resources/sap/ui/fl/changeHandler/BaseRename.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2021 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -18,7 +18,7 @@ sap.ui.define([
 	 * @constructor
 	 * @alias sap.ui.fl.changeHandler.BaseRename
 	 * @author SAP SE
-	 * @version 1.52.46
+	 * @version 1.52.47
 	 * @experimental Since 1.46
 	 */
 	var BaseRename = {
